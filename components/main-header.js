ac0042e15fc4432bad8ded625458eef9108c95af
Tri Nguyen <triminh.nguyen2098@gmail.com>
2025-02-07 15:35:51 +0700

    init project routing

---
+++ components/main-header.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function MainHeader() {
+  return <header id="main-header">
+    <div id="logo">
+      <Link href="/">Next News</Link>
+    </div>
+    <ul>
+      <li>
+        <Link href="/news">News</Link>
+      </li>
+    </ul>
+  </header>;
+}
